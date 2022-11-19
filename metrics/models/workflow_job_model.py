675f929fed1ad283abf0168ef13676b6f545a47a
gstimac <5202192+gstimac@users.noreply.github.com>
2022-11-19 20:47:41 +0100

    Deletes python and initializes golang project structure (#1)
    Co-authored-by: Goran Stimac <g.stimac>

--- metrics/models/workflow_job_model.py
+++
@@ -1,7 +0,0 @@
-
-class WorkflowJobModel:
-    def __init__(self, job, repository, workflow):
-        self.job = job
-        self.repository = repository
-        self.workflow = workflow
-
--- metrics/utils/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    def __init__(self, db_host: str, db_password: str, db_user: str, webhook_secret: str):
-        self.webhook_secret = webhook_secret
-        self.db_user = db_user
-        self.db_password = db_password
-        self.db_host = db_host
